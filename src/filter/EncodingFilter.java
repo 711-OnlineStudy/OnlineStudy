903ac153670c870273d25fc5e501b26060659c12
MrZeng <857165956@qq.com>
2017-09-02 19:21:40 +0800

    11

---
+++ src/filter/EncodingFilter.java
@@ -0,0 +1,53 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter("/EncodingFilter")
+public class EncodingFilter implements Filter {
+	String encoding=null;		// ×Ö·û±àÂë
+    /**
+     * Default constructor. 
+     */
+    public EncodingFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		encoding=null;
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		if(encoding!=null){
+			// ÉèÖÃrequest×Ö·û±àÂë
+			request.setCharacterEncoding(encoding);
+			// ÉèÖÃresponse×Ö·û±àÂë
+			response.setContentType("test/html;charset="+encoding);
+			
+		}
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		encoding=fConfig.getInitParameter("encoding");
+	}
+
+}
